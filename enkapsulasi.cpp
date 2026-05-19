5a899bc953cdfd21cdfb1b83d39460de09680da6
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-19 19:00:04 +0700

    enkapsulasi 14

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] =value;
+    }        
+    string getSkalarNo(int i) {
+        return saklarNo[i];
+    }
+};
